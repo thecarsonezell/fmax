93ba66d9ebbf8532274659a187bfbff566753d0e
thecarsonezell <116521151+thecarsonezell@users.noreply.github.com>
2023-04-07 13:50:07 -0400

    Added Frechet import

--- fmax/util/__init__.py
+++ fmax/util/__init__.py
@@ -2,4 +2,4 @@ from fmax.util.data_processing import get_min, get_max, jump_flat_split
 from fmax.util.random import get_random_fn
 from fmax.util.conversions import weibull_mu_and_sigma
 from fmax.util.metrics import mse
-from fmax.util.distributions import MinGumbel, MaxWeibull
+from fmax.util.distributions import MinGumbel, MaxWeibull, Frechet
