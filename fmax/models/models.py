5417b0dc519ba5a5ab11d25f82bf4812cfe175b9
thecarsonezell <116521151+thecarsonezell@users.noreply.github.com>
2023-04-10 23:11:39 -0400

    log priors

--- fmax/models/models.py
+++ fmax/models/models.py
@@ -92,13 +92,18 @@ class ForecastModel:
                 alpha_upper = prior_parameters['alpha']['upper']
                 scale_mean = prior_parameters['scale']['mean']
                 scale_std = prior_parameters['scale']['std']
-                
+
+                # Log-transform reparameterization
+                log_alpha = pm.Uniform('log_alpha', lower=np.log(alpha_lower), upper=np.log(alpha_upper))
+                log_scale = pm.Normal('log_scale', mu=scale_mean, sigma=scale_std)
+
+                alpha = pm.Deterministic('alpha', pm.math.exp(log_alpha))
+                scale = pm.Deterministic('scale', pm.math.exp(log_scale))
+
                 priors = {
-                    'alpha': pm.Uniform('alpha', lower=alpha_lower, upper=alpha_upper),
-                    'scale': pm.Normal('scale', mu=scale_mean, sigma=scale_std),
+                    'alpha': alpha,
+                    'scale': scale,
                 }
-
-
 
             # Get random sampling and likelihood for the kind of attempt
             loglike = fm.get_loglikelihood_fn(
